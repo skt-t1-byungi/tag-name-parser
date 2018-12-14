2689aaa3511620cd4a1025f8ae29415c0fdc3437
skt-t1-byungi <tiniwz@gmail.com>
2018-12-15 02:35:23 +0900

    add benchmark

--- README.md
+++ README.md
@@ -82,5 +82,14 @@ output:
 }
 ```
 
+## Benchmark
+Each test 10,000 times.
+
+tag-name-parser|htmlparser2|parse5|sax|html-parse-stringify2
+---|---|---|---|---
+~85 ms|~243 ms|~1.34 s|~458 ms|~333 ms
+
+The tag-name-parser has different purposes than other libraries. It does not claim to be more useful than other libraries through this benchmark.
+
 ## License
 MIT
---
+++ bench.js
@@ -0,0 +1,46 @@
+const bench = require('nanobench')
+const fs = require('fs')
+const tagNameParser = require('.')
+const htmlparser2 = require('htmlparser2')
+const parse5 = require('parse5')
+const sax = require('sax')
+const stringify2 = require('html-parse-stringify2')
+
+const str = fs.readFileSync('./bench_suit.txt', 'utf-8').trim()
+
+bench('tag-name-parser', b => {
+    b.start()
+    for (let i = 0; i < 10000; i++) tagNameParser(str)
+    b.end()
+})
+
+bench('htmlparser2', b => {
+    b.start()
+    for (let i = 0; i < 10000; i++) {
+        const parser = new htmlparser2.Parser()
+        parser.write(str)
+        parser.end()
+    }
+    b.end()
+})
+
+bench('parse5', b => {
+    b.start()
+    for (let i = 0; i < 10000; i++) parse5.parseFragment(str)
+    b.end()
+})
+
+bench('sax', b => {
+    b.start()
+    for (let i = 0; i < 10000; i++) {
+        const parser = sax.parser()
+        parser.write(str)
+    }
+    b.end()
+})
+
+bench('html-parse-stringify2', b => {
+    b.start()
+    for (let i = 0; i < 10000; i++) stringify2.parse(str)
+    b.end()
+})
