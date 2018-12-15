4adc4e036d776357baa2fae2662a803e542a51f3
skt-t1-byungi <tiniwz@gmail.com>
2018-12-15 15:50:53 +0900

    remove empty line

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,3 @@ declare function parse(str:string, opts?: Options): Node[]
 
 type Options = Partial<{strict: boolean, tag: [string, string]}>
 type Node = string | {name: string, single: true} | {name: string, single: false, children: Node[]}
-
