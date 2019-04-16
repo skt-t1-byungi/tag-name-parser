c147eb0bbd8d91842413214e0f2992f8bf95ef5c
skt-t1-byungi <tiniwz@gmail.com>
2019-04-16 22:47:12 +0900

    rollback d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export type Node = string | {name: string, single: true} | {name: string, single: false, children: Node[]}
+type Node = string | {name: string, single: true} | {name: string, single: false, children: Node[]}
 
 declare function parse(str:string, opts?: {strict?: boolean, tag?: [string, string]}): Node[]
 
